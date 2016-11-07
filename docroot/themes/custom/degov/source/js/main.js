5797e9dfa4b1ac1da16d1f9bef85d37dd71b2f62
raul <garcia@publicplan.de>
2016-11-07 11:23:58 +0100

    Added Theme from 1xInternet and fixed some errors. Uses of standard twig fucntions, filters and removed some unnecessaries functions.

--- docroot/themes/custom/degov/source/js/main.js
+++ docroot/themes/custom/degov/source/js/main.js
@@ -5,10 +5,20 @@ const Slick = require('slick-carousel');
 
 (function ($, Drupal) {
 
-  /*Drupal.behaviors.exampleBehavior = {
-    attach: (context) => {
-      $('.my-selector', context).once('example-behavior').each(() => {});
+  // Repeat parent element of dropdown as first element
+  Drupal.behaviors.dropdown = {
+    attach: function (context, settings) {
+      $('.navbar-nav').once('dropdown', function () {
+        console.info("ADFSASDF");
+        $(this).find('ul.dropdown-menu').each(function () {
+          var href = $(this).siblings('a').first().attr('href');
+          var text = $(this).siblings('a').first().text();
+          var link = '<a href="' + href + '">text</a>';
+          console.info("This is the line: " + link);
+          $(this).prepend('<li>' + link + '</li>');
+        });
+      });
     }
-  };*/
+  };
 
 })(jQuery, window.Drupal);
