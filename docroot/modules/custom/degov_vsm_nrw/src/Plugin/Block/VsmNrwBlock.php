6ae4808df0a486e6c3a7ab8a80afbe23bf2bb200
raul <garcia@publicplan.de>
2016-11-13 00:29:37 +0100

    Fixed Warnings due to non existent index in the result array. Fixed Warning due to pass non variables by reference.

--- docroot/modules/custom/degov_vsm_nrw/src/Plugin/Block/VsmNrwBlock.php
+++ docroot/modules/custom/degov_vsm_nrw/src/Plugin/Block/VsmNrwBlock.php
@@ -19,33 +19,33 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class VsmNrwBlock extends BlockBase {
 
- /**
- * {@inheritdoc}
- */
+  /**
+   * {@inheritdoc}
+   */
 
   public function build() {
 
     $q_area = 'land';
-    $q_search = null;
+    $q_search = NULL;
 
     // Module Path
     $modulePath = sprintf('modules/custom/degov_vsm_nrw/assets', \Drupal::root());
     $moduleRealPath = \Drupal::service('file_system')->realpath($modulePath);
 
     // Load Communes
-    $areas = null;
+    $areas = NULL;
     $areas = Yaml::parse(file_get_contents($moduleRealPath . '/areas.yml'));
 
     // retrieve GET variables
-    $request = null;
+    $request = NULL;
     $request = Request::createFromGlobals();
 
     // Query Parameter
-    $q = null;
+    $q = NULL;
     $q = $request->query->get('keys');
 
     // Clean Query Parameter
-    $q_arr = null;
+    $q_arr = NULL;
     $q_arr = explode(' ', mb_ereg_replace('[^\w\d]', ' ', mb_strtolower($q, 'UTF-8')));
 
     // Check each Areas
@@ -55,14 +55,14 @@ class VsmNrwBlock extends BlockBase {
       $area['result'] = 0;
 
       // Clean Area Name
-      $area_clean_arr = null;
+      $area_clean_arr = NULL;
       $area_clean_arr = explode(' ', mb_ereg_replace('[^\w\d]', ' ', mb_strtolower($area['name'], 'UTF-8')));
 
       // Check Words in Area Name with Words in Query Parameter
       foreach ($area_clean_arr as $area_clean_val) {
         foreach ($q_arr as $q_val) {
 
-          $perc = null;
+          $perc = NULL;
           similar_text($area_clean_val, $q_val, $perc);
           if ($perc >= 80) {
             $q_search[] = $q_val;
@@ -77,7 +77,7 @@ class VsmNrwBlock extends BlockBase {
     }
 
     // Hole eine Liste von Spalten
-    $tmp_result = null;
+    $tmp_result = NULL;
     foreach ($areas as $key => $row) {
       $tmp_result[$key] = $row['result'];
     }
@@ -89,36 +89,59 @@ class VsmNrwBlock extends BlockBase {
       $q_area = $areas[0]['key'];
     }
 
-    if ($q_search != null) $q_search = array_diff($q_arr, array_unique($q_search));
-    else $q_search = $q_arr;
+    if ($q_search != NULL) {
+      $q_search = array_diff($q_arr, array_unique($q_search));
+    }
+    else {
+      $q_search = $q_arr;
+    }
 
     // VSM NRW Seach
-    $result = null;
+    $result = NULL;
     $result = degov_vsm_nrw_search(join(' ', $q_search), $q_area);
     if (($result) && (!empty($result['resources']))) {
 
       // Generate HTML List
-      $list = null;
-      $list .= '<h2>'.t('Suchergebnisse der Verwaltungssuchmaschine NRW').'</h2>';
+      $list = NULL;
+      $list .= '<h2>' . t('Suchergebnisse der Verwaltungssuchmaschine NRW') . '</h2>';
       $list .= '<ol>';
       foreach ($result['resources'] as $entry) {
+        $renderableLink = Link::fromTextAndUrl(
+        strip_tags($entry['title']),
+        Url::fromUri($entry['url'],
+                      array(
+                        'attributes' => array('target' => array('_blank')),
+                        'external'   => TRUE,
+                      )
+                    )
+        )->toRenderable();
+
+        $renderableLink2 = Link::fromTextAndUrl(
+        strip_tags($entry['url']),
+        Url::fromUri($entry['url'],
+                      array(
+                       'attributes' => array('target' => array('_blank')),
+                       'external'   => TRUE,
+                      )
+                    )
+        )->toRenderable();
         $list .= '<li>';
-        $list .= '<h3>'.render(Link::fromTextAndUrl(strip_tags($entry['title']), Url::fromUri($entry['url'], array('attributes' => array('target' => array('_blank')), 'external' => true)))->toRenderable()).'</h3>';
-        $list .= '<p>'.$entry['description'].'</p>';
-        $list .= '<p><i>'.render(Link::fromTextAndUrl(strip_tags($entry['url']), Url::fromUri($entry['url'], array('attributes' => array('target' => array('_blank')), 'external' => true)))->toRenderable()).'</i></p>';
+        $list .= '<h3>' . render($renderableLink) . '</h3>';
+        $list .= '<p>' . $entry['description'] . '</p>';
+        $list .= '<p><i>' . render($renderableLink2) . '</i></p>';
         $list .= '</li>';
       }
       $list .= '</ol>';
 
       // Output Results as HTML
       return array(
-        '#type' => 'markup',
+        '#type'   => 'markup',
         '#markup' => $list,
-        '#cache' => [
+        '#cache'  => [
           'contexts' => [
             'url.query_args:keys',
           ],
-          'max-age' => 86400
+          'max-age'  => 86400,
         ],
       );
 
@@ -126,13 +149,13 @@ class VsmNrwBlock extends BlockBase {
 
     // No Results
     return array(
-      '#type' => 'markup',
-      '#markup' => '<h3>'.t('Die Verwaltungssuchmaschine NRW lieferte keine Ergebnisse.').'</h3>',
-      '#cache' => [
+      '#type'   => 'markup',
+      '#markup' => '<h3>' . t('Die Verwaltungssuchmaschine NRW lieferte keine Ergebnisse.') . '</h3>',
+      '#cache'  => [
         'contexts' => [
           'url.query_args:keys',
         ],
-        'max-age' => 86400
+        'max-age'  => 86400,
       ],
     );
 
