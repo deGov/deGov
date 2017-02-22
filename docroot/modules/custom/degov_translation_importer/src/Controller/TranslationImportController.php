783fa30634551ff6e26f9ae2134cf4b12ce32639
Christoph Breidert <c.breidert@1xinternet.de>
2017-02-22 10:44:14 +0100

    Removed themes and modules, because they are managed by composer now

--- docroot/modules/custom/degov_translation_importer/src/Controller/TranslationImportController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Drupal\degov_translation_importer\Controller;
-
-use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Language\LanguageManager;
-
-class TranslationImportController extends ControllerBase { }
