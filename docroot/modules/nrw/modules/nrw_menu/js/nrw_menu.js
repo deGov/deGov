f61bcab22375954b0ec8ebd303451b6505d1a664
Artem Dmitriiev <a.dmitriiev@1xinternet.de>
2017-07-05 09:39:56 +0000

    Merged in feature/AP-MIK1.1-rewised (pull request #21)
    Feature/AP MIK1.1 rewised
    
    * AP-MIK1.1 Menu templates
    
    * Add default js behaviors
    
    * Update dependencies
    
    * Add js and comments
    
    * Add extra field to menu item
    
    * Add comments
    
    * Add menu block subtitle
    
    * some template improvements

---
+++ docroot/modules/nrw/modules/nrw_menu/js/nrw_menu.js
@@ -0,0 +1,33 @@
+/**
+ * @file nrw_menu.js
+ *
+ * Defines the behavior of the NRW Menu module.
+ */
+(function ($, Drupal) {
+
+    'use strict';
+
+    /**
+     * Collapsible/Expandable menu behaviour
+     */
+    Drupal.behaviors.nrw_menu = {
+        attach: function (context, settings) {
+            // do nothing if there is no menu__header on the page
+            if ($('.nrw-menu__header', context).length == 0) {
+                return;
+            }
+            // on click add the class to sibling
+            $('.nrw-menu__header a', context).click(function (e) {
+                e.preventDefault();
+                // remove the class from all the content containers in the menu
+                $('.nrw-menu__content', context).each(function(){
+                    $(this).removeClass('is-expanded');
+                });
+                // add the class only to the sibling of the active menu__header
+                var parent = $(this).closest('.nrw-menu__col');
+                $('.nrw-menu__content', parent).addClass('is-expanded');
+            });
+        }
+    }
+
+})(jQuery, Drupal);
