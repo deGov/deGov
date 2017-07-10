ec85fd5c95039e96df247a825db9c5410fb10c4b
Fran <fran@Frans-Mac-mini.fritz.box>
2017-07-10 12:14:52 +0200

    add icons to menu

--- docroot/modules/nrw/modules/nrw_menu/js/nrw_menu.js
+++ docroot/modules/nrw/modules/nrw_menu/js/nrw_menu.js
@@ -17,19 +17,20 @@
                 return;
             }
             // on click add the class to siblings
-            $('.nrw-menu__col .nrw-menu__header', context).click(function (e) {
+            $('.nrw-menu-header__col .nrw-menu__header', context).click(function (e) {
                 e.preventDefault();
+                console.log('aaa');
                 // remove the class from all the content containers in the menu
                 // add the class only to the sibling of the active menu__header                
-                var parent = $(this).closest('.nrw-menu__col');
-                if( $(this).siblings('.nrw-menu__content').hasClass( "is-expanded" ) ){
+                var parent = $(this).closest('.nrw-menu-header__col');
+                if( $(this).siblings('.nrw-menu-header__content').hasClass( "is-expanded" ) ){
                     $(this).parent().removeClass('is-open');
-                    $(this).siblings('.nrw-menu__content').removeClass("is-expanded");
+                    $(this).siblings('.nrw-menu-header__content').removeClass("is-expanded");
                 }else {
-                    $('.nrw-menu__col').removeClass('is-open');
-                    $('.nrw-menu__content').removeClass("is-expanded");
+                    $('.nrw-menu-header__col').removeClass('is-open');
+                    $('.nrw-menu-header__content').removeClass("is-expanded");
                     $(this).parent().addClass('is-open');
-                    $(this).siblings('.nrw-menu__content').addClass("is-expanded");
+                    $(this).siblings('.nrw-menu-header__content').addClass("is-expanded");
                 }                
             });
             $('.nrw-menu__content-close a', context).click(function (e) {
