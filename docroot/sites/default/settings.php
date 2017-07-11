b98d870001f247b8c371da807def9de04dc94e95
Melinda Kovacs-Sztankovits <melinda@1xinternet.de>
2017-07-11 19:41:07 +0200

    general styling adjustments - header, node ref paragraph, paragraph headers, iframe par., link list ... RMNRW-115

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -41,3 +41,4 @@ if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+$settings['install_profile'] = 'degov_profile';
