7e200860b0e524f856e8aae560dae65f088c248a
Stefan Weber <s.weber@1xinternet.de>
2017-07-12 14:01:18 +0200

    settings back to normal

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -41,4 +41,3 @@ if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$settings['install_profile'] = 'degov_profile';
