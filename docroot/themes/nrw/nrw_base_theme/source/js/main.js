6536ce3f91e0713f5ffd20356a25beb158205e3a
Vivek <v.chauhan@1xinternet.de>
2017-07-13 18:56:56 +0530

    Design of slick slider

--- docroot/themes/nrw/nrw_base_theme/source/js/main.js
+++ docroot/themes/nrw/nrw_base_theme/source/js/main.js
@@ -190,4 +190,23 @@ const PhotoSwipeUiDefault = require('photoswipe/dist/photoswipe-ui-default');
       });
     }
   };
+
+  // Slick slider in press list
+  $('.view-latest-press .view-content').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
 })(jQuery, window.Drupal);
