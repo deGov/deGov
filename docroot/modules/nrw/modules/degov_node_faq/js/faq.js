f16dd7d27f4e720dd22e602f6c033daa3e01661b
Markus Orenstrat <markus.orenstrat@reinblau.de>
2017-07-20 12:20:13 +0200

    Add stub for JS library.

---
+++ docroot/modules/nrw/modules/degov_node_faq/js/faq.js
@@ -0,0 +1,19 @@
+/**
+ * @file faq.js
+ *
+ * Defines the behavior of the FAQ search page.
+ */
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
+
+  /**
+   * Expands all search results that have the search term in title or body.
+   */
+  Drupal.behaviors.degov_node_faq = {
+    attach: function (context, settings) {
+      // @todo Implement the behavior.
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
