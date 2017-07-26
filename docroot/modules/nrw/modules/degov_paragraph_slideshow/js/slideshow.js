aead51bb9baedf7cb18519d45b2c581d24e4bae0
v-chauhan <v.chauhan@1xinternet.de>
2017-07-26 19:19:46 +0530

    Slider type 2

--- docroot/modules/nrw/modules/degov_paragraph_slideshow/js/slideshow.js
+++ docroot/modules/nrw/modules/degov_paragraph_slideshow/js/slideshow.js
@@ -16,6 +16,50 @@
         dots: true,
         autoplay: true
       });
+
+      // Slick slider for prev/next thumbnails images
+      $('.slideshow-with-prev-next .slides_prev_next').slick({
+        dots: true,
+        slidesToShow: 1,
+        autoplay: false
+      });
+      setTimeout(function() {
+        $('.slideshow-with-prev-next .slides_prev_next').prepend('<div class="prev-slick-img slick-thumb-nav"><img src="/degov_july/docroot/sites/default/files/styles/medium/public/Free-Wallpaper-Nature-Scenes_0.jpg" class="img-responsive"></div>').append('<div class="next-slick-img slick-thumb-nav"><img src="/degov_july/docroot/sites/default/files/styles/medium/public/Free-Wallpaper-Nature-Scenes_0.jpg" class="img-responsive"></div>');
+        get_prev_slick_img();
+        get_next_slick_img();
+      }, 500);
+
+      $('.slides_prev_next').on('click', '.slick-prev', function() {
+        get_prev_slick_img();
+      });
+      $('.slides_prev_next').on('click', '.slick-next', function() {
+        get_next_slick_img();
+      });
+      $('.slideshow-with-prev-next .slides_prev_next').on('swipe', function(event, slick, direction) {
+        if (direction == 'left') {
+          get_prev_slick_img();
+        }
+        else {
+          get_next_slick_img();
+        }
+      });
+      function get_prev_slick_img() {
+        // For prev img
+        var prev_slick_img = $('.slick-current').prev('.slide').find('.slide__media img').attr('src');
+        $('.prev-slick-img img').attr('src', prev_slick_img);
+        // For next img
+        var prev_next_slick_img = $('.slick-current').next('.slide').find('.slide__media img').attr('src');
+        $('.next-slick-img img').attr('src', prev_next_slick_img);
+      }
+      function get_next_slick_img() {
+        // For next img
+        var next_slick_img = $('.slick-current').next('.slide').find('.slide__media img').attr('src');
+        $('.next-slick-img img').attr('src', next_slick_img);
+        // For prev img
+        var next_prev_slick_img = $('.slick-current').prev('.slide').find('.slide__media img').attr('src');
+        $('.prev-slick-img img').attr('src', next_prev_slick_img);
+      }
+      // End
     }
   }
 
--- docroot/themes/nrw/nrw_base_theme/public/js/bundle.js
+++ docroot/themes/nrw/nrw_base_theme/public/js/bundle.js
@@ -285,26 +285,6 @@
 	    }
 	  };
 
-	  // Slick slider in press list
-	  Drupal.behaviors.slickPress = {
-	    attach: function attach(context, settings) {
-	      $('.view-latest-press .view-content').slick({
-	        dots: true,
-	        infinite: false,
-	        speed: 300,
-	        slidesToShow: 3,
-	        slidesToScroll: 3,
-	        responsive: [{
-	          breakpoint: 992,
-	          settings: {
-	            slidesToShow: 1,
-	            slidesToScroll: 1
-	          }
-	        }]
-	      });
-	    }
-	  };
-
 	  // Check heigh of image in contact person
 
 	  Drupal.behaviors.contactHeight = {
