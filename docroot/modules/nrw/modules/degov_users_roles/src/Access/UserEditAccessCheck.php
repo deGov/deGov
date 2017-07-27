bcbf5fe137b98895fa1d4549552dfb245eeb907f
Artem Dmitriiev <a.dmitriiev@1xinternet.de>
2017-07-27 15:50:34 +0200

    RMNRW-102 role delegation and permissions

---
+++ docroot/modules/nrw/modules/degov_users_roles/src/Access/UserEditAccessCheck.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\degov_users_roles\Access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Routing\Access\AccessInterface;
+use Drupal\Core\Routing\RouteMatch;
+use Drupal\Core\Session\AccountProxy;
+use Drupal\user\UserInterface;
+
+/**
+ * Class UserEditAccessCheck
+ *
+ * @package Drupal\degov_users_roles\Access
+ */
+class UserEditAccessCheck implements AccessInterface {
+
+  /**
+   * A custom access check.
+   *
+   * @param \Drupal\Core\Session\AccountProxy $account
+   *   Run access checks for this account.
+   *
+   * @param \Drupal\Core\Routing\RouteMatch $route_match
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   */
+  public function access(AccountProxy $account, RouteMatch $route_match) {
+    /** @var \Drupal\user\UserInterface $userToEdit */
+    $userToEdit = $route_match->getParameter('user');
+    if ($userToEdit && $userToEdit instanceof UserInterface && $userToEdit->hasRole('administrator')) {
+      $current_user_roles = $account->getAccount()->getRoles();
+      return AccessResult::allowedIf(in_array('administrator', $current_user_roles));
+    }
+
+    return AccessResult::allowed();
+  }
+}
