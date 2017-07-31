15f4bb5450ecbb48e731fcf55bdb02369b7cf068
Artem Dmitriiev <a.dmitriiev@1xinternet.de>
2017-07-31 13:13:15 +0200

    Calendar widget layout and default arguments

--- docroot/modules/nrw/modules/degov_node_event/js/events.js
+++ docroot/modules/nrw/modules/degov_node_event/js/events.js
@@ -20,6 +20,9 @@
                 var date = parent.attr('id').replace('events-', '');
                 var view_wrapper = $(this).closest('.view-display-id-events_page_list');
                 $('input[name="from"]', view_wrapper).val(date);
+                var toDate = new Date(date);
+                toDate.setDate(toDate.getDate() + 1);
+                $('input[name="to"]', view_wrapper).val(toDate.toISOString().substr(0, 10));
                 $('form', view_wrapper).submit();
             });
         }
