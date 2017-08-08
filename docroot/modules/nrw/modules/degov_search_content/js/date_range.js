9aa1df3bdfecfcc1c42ee2d7f046bf82cf2e49db
Nicky Vandevoorde <nicky.vandevoorde@gmail.com>
2017-08-08 10:02:57 +0200

    Fixes on date widget and refactor

--- docroot/modules/nrw/modules/degov_search_content/js/date_range.js
+++ docroot/modules/nrw/modules/degov_search_content/js/date_range.js
@@ -5,28 +5,27 @@
 
 (function ($) {
 
-    'use strict';
+  'use strict';
 
-
-    Drupal.facets = Drupal.facets || {};
-    Drupal.behaviors.dateInputRange = {
-        attach: function (context, settings) {
-            Drupal.facets.dateInputRange(context, settings);
-        }
-    };
-
-    Drupal.facets.dateInputRange = function (context, settings) {
-        if (typeof settings.dateFilter != 'undefined') {
-            $('.date-filter-wrapper .date-from').val(settings.dateFilter.min);
-            $('.date-filter-wrapper .date-to').val(settings.dateFilter.max);
-        }
-        $('.date-filter').on('click', function () {
-            var dateFrom = $('.date-filter-wrapper .date-from').val();
-            var dateTo = $('.date-filter-wrapper .date-to').val();
-            var href = settings.dateFilter.facetUrl.replace('date_min', dateFrom);
-            href = href.replace('date_max', dateTo);
-            window.location.href = href;
-        });
+  Drupal.facets = Drupal.facets || {};
+  Drupal.behaviors.dateInputRange = {
+    attach: function (context, settings) {
+      Drupal.facets.dateInputRange(context, settings);
     }
+  };
+
+  Drupal.facets.dateInputRange = function (context, settings) {
+    if (typeof settings.dateFilter != 'undefined') {
+      $('.date-filter-wrapper .date-from').val(settings.dateFilter.min);
+      $('.date-filter-wrapper .date-to').val(settings.dateFilter.max);
+    }
+    $('.date-filter').on('click', function () {
+      var dateFrom = $('.date-filter-wrapper .date-from').val();
+      var dateTo = $('.date-filter-wrapper .date-to').val();
+      var href = settings.dateFilter.facetUrl.replace('date_min', dateFrom);
+      href = href.replace('date_max', dateTo);
+      window.location.href = href;
+    });
+  }
 
 })(jQuery);
