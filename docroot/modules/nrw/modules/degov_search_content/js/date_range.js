fd5f0f55caf139014d2874b2aafdb023219984cb
Fran <fran@Frans-Mac-mini.fritz.box>
2017-08-10 18:29:50 +0200

    added * if one of the date fields is empty

--- docroot/modules/nrw/modules/degov_search_content/js/date_range.js
+++ docroot/modules/nrw/modules/degov_search_content/js/date_range.js
@@ -23,30 +23,15 @@
     $('.date-filter').on('click', function () {
       var dateFrom = $('.date-filter-wrapper .date-from').val();
       var dateTo = $('.date-filter-wrapper .date-to').val();
+      if(dateFrom == ''){
+        dateFrom = '*';
+      }
+      if(dateTo == ''){
+        dateTo = '*';
+      }
       var href = settings.dateFilter.facetUrl.replace('date_min', dateFrom);
       href = href.replace('date_max', dateTo);
       window.location.href = href;
-    });
-
-    $('.date-filter').click(function(){
-      if($('.date-to').val() == ''){
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1; 
-        var yyyy = today.getFullYear();
-        if(dd<10) 
-        {
-            dd='0'+dd;
-        } 
-
-        if(mm<10) 
-        {
-            mm='0'+mm;
-        }
-        today = yyyy+'-'+mm+'-'+dd;
-        $('.date-to').val(today);
-      }
-      
     });
   }
 
