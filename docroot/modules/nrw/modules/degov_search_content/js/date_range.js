b1c9d9a2c70460ca4b0f4c9882b8589e9e6e594b
Fran <fran@Frans-Mac-mini.fritz.box>
2017-08-10 12:16:50 +0200

    RMNRW-156 - Compiled files and js for datepicker

--- docroot/modules/nrw/modules/degov_search_content/js/date_range.js
+++ docroot/modules/nrw/modules/degov_search_content/js/date_range.js
@@ -33,6 +33,27 @@
       href = href.replace('date_max', dateTo);
       window.location.href = href;
     });
+
+    $('.date-filter').click(function(){
+      if($('.date-to').val() == ''){
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; 
+        var yyyy = today.getFullYear();
+        if(dd<10) 
+        {
+            dd='0'+dd;
+        } 
+
+        if(mm<10) 
+        {
+            mm='0'+mm;
+        }
+        today = yyyy+'-'+mm+'-'+dd;
+        $('.date-to').val(today);
+      }
+      
+    });
   }
 
 })(jQuery);
