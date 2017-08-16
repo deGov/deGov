2ad80ec9ff65ec5ba1de2d25bccec6662ebdb51f
Nicky Vandevoorde <nicky.vandevoorde@gmail.com>
2017-08-16 18:11:53 +0200

    Moves route ajax view mode request to degov_common

--- docroot/modules/nrw/modules/degov_common/src/Common.php
+++ docroot/modules/nrw/modules/degov_common/src/Common.php
@@ -128,4 +128,34 @@ class Common {
     }
   }
 
+  /**
+   * Returns an array of allowed view modes by given entity type and id.
+   *
+   * @param string $entity_type
+   *   Entity type that needs is checked for allowed view modes.
+   * @param string $entity_id
+   *   Entity id that needs is checked for allowed view modes.
+   *
+   * @return array
+   *   An array of allowed view modes.
+   */
+  public static function ajaxEntityLoad($entity_type, $entity_id) {
+    // Retrieve the entity.
+    $entity_storage = \Drupal::entityTypeManager()->getStorage($entity_type);
+    if (!empty($entity_storage)) {
+      $entity = $entity_storage->load($entity_id);
+      // Check the entity for enabled view modes.
+      $view_modes = \Drupal::entityQuery('entity_view_display')
+        ->condition('bundle', $entity->bundle())
+        ->execute();
+      foreach ($view_modes as $view_mode) {
+        $view_mode = explode('.', $view_mode);
+        if (!empty($view_mode[2])) {
+          $allowed_view_modes[] = $view_mode[2];
+        }
+      }
+    }
+
+    return new JsonResponse($allowed_view_modes);
+  }
 }
